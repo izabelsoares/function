c64dd0b8ff74042f33fbe79043c372f72dc2dc20
izabelsoares <izabelsoaresbds@gmail.com>
2021-05-18 19:42:15 -0300

    tarefa de function concluida

---
+++ script.js
@@ -0,0 +1,28 @@
+function mensagem(){
+    console.log("Olá equipe do VnW :)")
+}
+mensagem()
+
+
+function nome(){
+   console.log("Izabel")
+}
+nome()
+
+
+function sobreMim(nome, idade, estilomusical) {
+    console.log(`Olá, meu nome é ${nome}, tenho ${idade} e meu estilo musical preferido é ${estilomusical}`)
+}
+sobreMim("Izabel", 18, "r&b")
+
+
+function sobreMim2(filme, musica) {
+    console.log(`Meu filme favorito é ${filme} e minha música favorita é ${musica}`)
+}
+sobreMim2("Castelo Animado", "Morning Glory")
+
+
+function tripleN(num){
+    return num * 3
+}
+console.log(tripleN(15))
